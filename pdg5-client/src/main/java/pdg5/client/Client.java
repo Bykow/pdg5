d3d17d066a14a7753fc658c8cae2f17577c51581
Andrea Cotza <andrea@cotza.me>
2017-10-03 10:36:39 +0200

    Setup JavaFx main view

--- pdg5-client/src/main/java/pdg5/client/Client.java
+++ pdg5-client/src/main/java/pdg5/client/Client.java
@@ -1,7 +1,48 @@
 package pdg5.client;
 
-public class Client {
-    public static void main(String... args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class Client extends Application {
+
+    private Stage primaryStage;
+    private SplitPane rootLayout;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("WordOn Desktop");
+
+        initRootLayout();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Client.class.getResource("/fxml/mainView.fxml"));
+            rootLayout = loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

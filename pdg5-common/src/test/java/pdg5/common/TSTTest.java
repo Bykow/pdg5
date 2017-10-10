13e924a68ebd4511098513810f33e07cf594e876
Lawrence <bykow12345@gmail.com>
2017-10-10 10:50:06 +0200

    TsT tree test ok

---
+++ pdg5-common/src/test/java/pdg5/common/TSTTest.java
@@ -0,0 +1,45 @@
+package pdg5.common;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created on 10.10.17 by Bykow
+ */
+public class TSTTest {
+    private static TST tree = new TST();
+
+    @BeforeClass
+    public static void init() throws Exception {
+        long start = System.nanoTime();
+        ClassLoader classLoader = TSTTest.class.getClassLoader();
+
+        Stream<String> lines = Files.lines(Paths.get(classLoader.getResource("dico/french.dic").toURI()));
+        lines.forEach(e -> tree.put(e));
+        lines.close();
+
+        long stop = System.nanoTime();
+        System.out.println("Created in : " + ((stop - start) / 1000000 ) + " ms");
+    }
+
+    @Test
+    public void put() throws Exception {
+        tree.put("derp123");
+        assertTrue(tree.contains("derp123"));
+    }
+
+    @Test
+    public void contains() throws Exception {
+        assertTrue(tree.contains("ab"));
+        assertTrue(tree.contains("aigle"));
+    }
+}

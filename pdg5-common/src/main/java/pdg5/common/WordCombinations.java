421e5f471e768d882b2e61436a9c957a53549362
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2017-11-12 12:36:11 +0100

    implements joker handling in algorithm

--- pdg5-common/src/main/java/pdg5/common/WordCombinations.java
+++ pdg5-common/src/main/java/pdg5/common/WordCombinations.java
@@ -6,36 +6,40 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 
+ *
  */
 public class WordCombinations {
 
-   TST dictionnary;
-   StringBuilder sb;
-   Map<Character, Integer> letterMap;
-   List<String> listOfWords;
+   private final char JOKER = '?';
+   private final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+   private final TST dictionnary;
+   private StringBuilder sb;
+   private Map<Character, Integer> letterMap;
+   private List<String> listOfWords;
 
    public WordCombinations(TST dictionnary) {
       this.dictionnary = dictionnary;
    }
 
    /**
-    * get All possible words we can do with 
+    * get All possible words we can do with
+    *
     * @param letters
-    * @return 
+    * @return
     */
    public List<String> getAllWordsFromLetters(String letters) {
       listOfWords = new ArrayList<>();
       sb = new StringBuilder();
       letterMap = new HashMap<>(letters.length());
-      
+
       for (char c : letters.toCharArray()) {
          Integer val = letterMap.putIfAbsent(c, 1);
-         if(val != null){
+         if (val != null) {
             letterMap.replace(c, val + 1);
          }
       }
-      
+
       search(letters.length());
       return listOfWords;
    }
@@ -47,16 +51,25 @@ public class WordCombinations {
             Integer number = entry.getValue();
 
             if (number > 0) {
-               sb.append(letter);
-               if (sb.length() >= 2 && dictionnary.contains(sb.toString())) {
-                  listOfWords.add(sb.toString());
+               String lettersToTest;
+               if (letter == JOKER) {
+                  lettersToTest = ALPHABET;
+               } else {
+                  lettersToTest = letter.toString();
                }
-               
-               entry.setValue(number - 1);
-               search(length - 1);
-               entry.setValue(number);
-               
-               sb.deleteCharAt(sb.length() - 1);
+
+               for (char c : lettersToTest.toCharArray()) {
+                  sb.append(c);
+                  if (sb.length() >= 2 && dictionnary.contains(sb.toString())) {
+                     listOfWords.add(sb.toString());
+                  }
+
+                  entry.setValue(number - 1);
+                  search(length - 1);
+                  entry.setValue(number);
+
+                  sb.deleteCharAt(sb.length() - 1);
+               }
             }
          });
       }

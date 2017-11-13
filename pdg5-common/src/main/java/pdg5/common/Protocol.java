8ecac803bb48cca9295c73a21723457e3d5975c9
Maxime Guillod <maxime@proteck.ch>
2017-11-13 15:36:04 +0100

    ;)

--- pdg5-common/src/main/java/pdg5/common/Protocol.java
+++ pdg5-common/src/main/java/pdg5/common/Protocol.java
@@ -8,7 +8,7 @@ public interface Protocol {
     /**
      * Socket
      */
-    final String DEFAULT_SERVER = "192.168.99.100"; // modify for tests
+    final String DEFAULT_SERVER = "127.0.0.1"; // modify for tests
     final int DEFAULT_PORT = 44555;
 
     final int NUMBER_OF_TUILES_PER_PLAYER = 7;

a639230a44820c11390ffea5a93879eb35bca2b4
jimmyVerdasca <jimmy.verdasca@heig-vd.ch>
2017-11-28 09:18:21 +0100

    back up test

--- pdg5-server/src/main/java/pdg5/server/process/ProcessSignIn.java
+++ pdg5-server/src/main/java/pdg5/server/process/ProcessSignIn.java
@@ -29,7 +29,7 @@ public class ProcessSignIn implements GenericProcess {
 
     @Override
     public Message execute() {
-       /* 
+       /*
        if(manageUser.isCorrectPassword(signIn.getUsername(), signIn.getPassword())) {
             //return new Load(manageGame(signIn.getUsername()));
             //todo waiting for game logic to improve
@@ -39,8 +39,8 @@ public class ProcessSignIn implements GenericProcess {
         } else {
             return new ErrorMessage("Password invalid in SignIn for user " + signIn.getUsername());
         }
-        */
-       
+        
+       */
        return null;
     }
 }

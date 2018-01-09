0817ce16b60bffe376de83c6da782931350e9cc6
Andrea Cotza <cotza@protonmail.ch>
2018-01-09 11:36:47 +0100

    Finished game list UI in loby

--- pdg5-client/src/main/java/pdg5/client/controller/LobyController.java
+++ pdg5-client/src/main/java/pdg5/client/controller/LobyController.java
@@ -2,10 +2,21 @@ package pdg5.client.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import pdg5.client.ClientSender;
+import pdg5.client.view.GGameListEntry;
+import pdg5.common.game.GameModel;
+import pdg5.common.protocol.Game;
 import pdg5.common.protocol.NewGame;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class LobyController {
 
@@ -13,12 +24,22 @@ public class LobyController {
     private Label titleWaiting;
     private Label titleFinished;
 
+    private ArrayList<Game> gameModelList;
+
+    private GGameListEntry selected;
+
     private ClientSender sender;
     
     @FXML
     private VBox gameList;
 
     public LobyController(ClientSender sender) {
+        gameModelList = new ArrayList<>();
+
+        gameModelList.add(new Game(0, new Date(), new Date(), 0, null, null, 12, null, 56, true, GameModel.State.IN_PROGRESS));
+        gameModelList.add(new Game(0, new Date(), new Date(), 0, null, null, 12, null, 56, true, GameModel.State.IN_PROGRESS));
+        gameModelList.add(new Game(0, new Date(), new Date(), 0, null, null, 12, null, 56, true, GameModel.State.FINISHED));
+
         titleToPlay = new Label("A ton tour");
         titleToPlay.getStyleClass().add("titleToPlay");
         titleWaiting = new Label("En attente");
@@ -35,14 +56,63 @@ public class LobyController {
     }
 
     private void refresh() {
+        gameList.getChildren().clear();
+
         gameList.getChildren().add(titleToPlay);
+        gameList.getChildren().addAll(gameModelList.stream()
+                .filter(g -> g.isYourTurn() && g.getState() == GameModel.State.IN_PROGRESS)
+                .sorted(Comparator.comparing(Game::getLastActivity))
+                .map(m -> new GGameListEntry(m, this::handleMouseClick, this::handleDelete))
+                .collect(Collectors.toList()));
+
         gameList.getChildren().add(titleWaiting);
+        gameList.getChildren().addAll(gameModelList.stream()
+                .filter(g -> !g.isYourTurn() && g.getState() == GameModel.State.IN_PROGRESS)
+                .sorted(Comparator.comparing(Game::getLastActivity))
+                .map(m -> new GGameListEntry(m, this::handleMouseClick, this::handleDelete))
+                .collect(Collectors.toList()));
+
         gameList.getChildren().add(titleFinished);
+        gameList.getChildren().addAll(gameModelList.stream()
+                .filter(g -> g.getState() == GameModel.State.FINISHED)
+                .sorted(Comparator.comparing(Game::getLastActivity))
+                .map(m -> new GGameListEntry(m, this::handleMouseClick, this::handleDelete))
+                .collect(Collectors.toList()));
+
+        if(selected != null) {
+            Optional<Node> selBefRefresh = gameList.getChildren().stream()
+                    .filter(g -> ((GGameListEntry) g).getModel().getID() == selected.getModel().getID())
+                    .findFirst();
+            if (selBefRefresh.isPresent()) {
+                selected = (GGameListEntry) selBefRefresh.get();
+                selected.setSelected(true);
+            } else {
+                selected = null;
+            }
+        }
+    }
+
+    private void unselectLast() {
+        if(selected != null)
+            selected.setSelected(false);
     }
 
     @FXML
     private void startNewGame(ActionEvent actionEvent) {
         System.out.println("new game");
         sender.add(new NewGame());
+    }
+
+    private void handleMouseClick(MouseEvent event) {
+        GGameListEntry element = (GGameListEntry)event.getSource();
+        unselectLast();
+        selected = element;
+        element.setSelected(true);
+    }
+
+    private void handleDelete(ActionEvent event) {
+        Node btn = (Node)event.getSource();
+        GGameListEntry element = (GGameListEntry)btn.getParent();
+        System.out.println("delete request for " + element.getModel().getID());
     }
 }

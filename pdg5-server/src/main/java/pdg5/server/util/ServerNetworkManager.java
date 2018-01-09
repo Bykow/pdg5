447ea5c04901a88f7484625e0bc440b17703212f
Lawrence <bykow12345@gmail.com>
2018-01-09 16:05:10 +0100

    merge fixed

--- pdg5-server/src/main/java/pdg5/server/util/ServerNetworkManager.java
+++ pdg5-server/src/main/java/pdg5/server/util/ServerNetworkManager.java
@@ -41,12 +41,12 @@ public class ServerNetworkManager {
             while (true) {
                 socket = serverSocket.accept();
 
-                SSLSocketFactory sslSf = sslContext.getSocketFactory();
-                // The host name doesn't really matter, since we're turning it into a server socket
-                // (No need to match the host name to the certificate on this side).
-                SSLSocket sslSocket = (SSLSocket) sslSf.createSocket(socket, null,
-                        socket.getPort(), false);
-                sslSocket.setUseClientMode(false);
+//                SSLSocketFactory sslSf = sslContext.getSocketFactory();
+//                // The host name doesn't really matter, since we're turning it into a server socket
+//                // (No need to match the host name to the certificate on this side).
+//                SSLSocket sslSocket = (SSLSocket) sslSf.createSocket(socket, null,
+//                        socket.getPort(), false);
+//                sslSocket.setUseClientMode(false);
 
                 // Use the sslSocket InputStream/OutputStream as usual.
                 new Thread(new ClientHandler(socket, activeUser, gameController)).start();

fdc3257c9323dc8b1410c4bc1360f225cc0e42c5
Lawrence <bykow12345@gmail.com>
2018-01-14 17:54:42 +0100

    other small fix

--- pdg5-client/src/main/java/pdg5/client/controller/GameController.java
+++ pdg5-client/src/main/java/pdg5/client/controller/GameController.java
@@ -496,6 +496,7 @@ public class GameController extends AbstractController {
             temp.add(((GTile) st.getChildren().get(1)).getModel());
         }
         Collections.shuffle(temp);
+        cleanList(userList);
         updateList(temp, deckList);
     }
 
